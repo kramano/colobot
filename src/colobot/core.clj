cb8b2203d19005d50d71900d442063a26c60add3
Maxim Novik <max.rindon@gmail.com>
2013-09-30 10:37:29 +0400

    Added alter-ego as AI engine, added browser interaction.

--- src/colobot/core.clj
+++ src/colobot/core.clj
@@ -1,6 +1,7 @@
 (ns colobot.core
   (:gen-class)
-  (:require [clj-http.client :as client]))
+  (:require [clj-http.client :as client]
+            [alter-ego.core :refer :all]))
 
 (defn create-colobot []
   ({}))
@@ -20,9 +21,14 @@
 (defn dead? [hero]
   (= 0 (:health hero)))
 
-(defn posiible-enough-gold [hero]
-  (let [gold-string (:gold_approx hero)]
-    ()))
+(defn maybe-enough-gold? [hero]
+  (let [gold-string (:gold_approx hero)
+        gld-v (re-find #"(\d+) сотен|(\d+) тысяч" gold-string)
+        hundreds (nth gld-v 1 -1)
+        thousands (nth gld-v 2 -1)]
+    (if hundreds
+      (<= 30 (Integer. hundreds))
+      (<= 3 (Integer. thousands)))))
 
 (defn low-godpower? [hero]
   (< (:godpower hero) 25))
@@ -30,15 +36,38 @@
 (defn in-town? [hero]
   (not (clojure.string/blank? (:town_name hero))))
 
-(defn fighting? [hero]
+(defn fighting-with-boss? [hero]
   (:arena_fight hero))
 
-(defn collect-gld-strings [names]
-  (let [res []]
-    (pmap #(->> (get-hero-state %)
-                      (:gold_approx)
-                      (conj res))
-          names)))
+
+(defn tree [hero]
+  (selector "Root"
+   (sequence "Ressurect if dead"
+             (action "Is hero dead?"
+                     (dead? hero))
+             (action "Ressurect hero"
+                     (println "Rssurect")))
+   (sequence "Hero is in town"
+             (action "Is hero in town?"
+                     (in-town? hero))
+             (action "Maybe has gold for brick?"
+                     (maybe-enough-gold? hero))
+             (action "Check actual gold"
+                     (println "Check gold"))
+             (action "Try to make a brick behavior"
+                     (println "Try to make a brick behavior")))
+   (sequence "Hero is fighting with boss"
+             (action "Is hero fighting?"
+                     (fighting-with-boss? hero))
+             (action "Fighting behavior"
+                     (println "Fighting behavior")))
+   (selector "Messing around"
+             (sequence "Hero is fighting"
+                       (action "Check if fighting"
+                               (println "Check if fighting")))
+             (sequence "Ok"
+                       (action "Try to dig a treasure"
+                               (println "Try to dig a treasure"))))))
 
 (defn -main
   "I don't do a whole lot ... yet."
